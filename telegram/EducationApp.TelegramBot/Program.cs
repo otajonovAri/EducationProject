07332665a00fd9f377830b69dae937155fab5d3c
OneWay2Go <uchqunov.m@yandex.com>
2025-08-14 18:48:42 +0500

    TelegramBot console app created

---
+++ telegram/EducationApp.TelegramBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationApp.TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
